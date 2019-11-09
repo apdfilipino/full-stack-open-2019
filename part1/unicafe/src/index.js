b590c89bbd52cbfeb1ebebc4d9a5081bcc21babd
Aaron Paul Filipino <apdfilipino@gmail.com>
2019-11-10 00:25:19 +0800

    Part 1, Unicafe 1.11 done

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -7,13 +7,46 @@ const App = () => {
     const [neutral, setNeutral] = useState(0)
     const [bad, setBad] = useState(0)
 
+    const all = good + neutral + bad
+    const average = (all === 0) ? 0 : (good - bad) / all
+    const positive = (all === 0) ? 0 : good / all    
+
+    const statistics = [
+        {
+            name: "good",
+            value: good
+        },
+        {
+            name: "neutral",
+            value: neutral
+        },
+        {
+            name: "bad",
+            value: bad
+        },        
+        {
+            name: "all",
+            value: all
+        },
+        {
+            name: "average",
+            value: average
+        },
+        {
+            name: "positive",
+            value: positive + "%"
+        }
+    ]
+
+    // console.log(statistics)
+
     return(
         <div>
             <Header />
             <Button name="good" onClick={() => setGood(good + 1)} />
             <Button name="neutral" onClick={() => setNeutral(neutral + 1)} />
             <Button name="bad" onClick={() => setBad(bad + 1)} />
-            <Total values={[good, neutral, bad]} />
+            <Statistics statistics={statistics} />
         </div>
     )
 }
@@ -28,21 +61,47 @@ const Button = ({ name, onClick }) => {
     )
 }
 
-const Total = ({values}) => {
-    let all = values[0] + values[1] + values[2]
-    let average = (all == 0) ? 0 : (values[0] - values[2]) / all
-    let positive = (all == 0) ? 0 : values[0] / all
+const Statistics = (props) => {
+    let [good, neutral, bad, all, average, positive] = props.statistics
 
+    if(all.value === 0){
+        return (
+            <>
+                <h1>statistics</h1>
+                <p>No feedback given</p>
+            </>
+        )
+    }
+    else{        
+        return (
+            <>
+                <h1>statistics</h1>
+                <table>
+                    <tbody>
+                        <Statistic statistic={good} />
+                        <Statistic statistic={neutral} />
+                        <Statistic statistic={bad} />
+                        <Statistic statistic={all} />
+                        <Statistic statistic={average} />
+                        <Statistic statistic={positive} />
+                    </tbody>
+                </table>
+            </>
+        )
+    }
+}
+
+const Statistic = ( {statistic} ) => {
     return (
-        <p>
-            good {values[0]} <br />
-            neutral {values[1]} <br />
-            bad {values[2]}  <br />
-            all {all} <br />
-            average {average} <br />
-            positive {positive}%
-        </p>
-    )
+        <tr>
+            <td>
+                {statistic.name}
+            </td>
+            <td>
+                {statistic.value}
+            </td>
+        </tr>
+    )    
 }
 
 

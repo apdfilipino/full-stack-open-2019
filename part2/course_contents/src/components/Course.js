1d41724641be74c7519ab7583da9e5c4cafa97cc
Aaron Paul Filipino <apdfilipino@gmail.com>
2019-11-11 00:28:21 +0800

    Part 2, course_contents Completed

---
+++ part2/course_contents/src/components/Course.js
@@ -0,0 +1,52 @@
+import React from 'react'
+
+const Course = ({ course }) => {
+    return(
+        <div>
+            <Header name = {course.name} />
+            <Content parts = {course.parts} />
+        </div>
+    )
+
+}
+
+const Header = ({name}) => {
+    return (
+        <h1>
+            {name}
+        </h1>
+    )
+}
+
+const Content = ({parts}) => {
+    const total = parts.reduce( (sum, part) => sum += part.exercises, 0)
+
+    return (
+        <div>
+            {parts.map( part => <Part key={part.id} name= {part.name} exercises={part.exercises} />)}
+            <Total total={total} />
+        </div>
+    )
+}
+
+const Total = ({total}) => {
+    return (
+        <div>
+            <b>total of {total} exercises</b>
+        </div>
+    )
+}
+
+const Part = ({name, exercises}) => {
+    return (
+        <>
+            <div>
+                {name} {exercises}
+            </div>
+            <br />
+        </>        
+    )
+}
+
+
+export default Course

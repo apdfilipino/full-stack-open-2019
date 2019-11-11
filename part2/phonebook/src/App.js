1bec293b6daee55ab9d1c92c14192c879aee334c
Aaron Filipino <apdfilipino@gmail.com>
2019-11-11 19:15:00 +0100

    Part 2.11, Phonebook Completed

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,21 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+
+import axios from 'axios';
+
 import PersonForm from './components/PersonForm';
 import Filter from './components/Filter';
 import Person from './components/Person';
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
 
+  const [ persons, setPersons ] = useState([])
   const [ name, setName ] = useState('')
   const [ phoneNumber, setPhoneNumber ] = useState('')
   const [ filter, setFilter ] = useState('')
 
-  const filteredPersons = persons.filter(f => f.name.includes(filter));
+  const filteredPersons = persons.filter(f => f.name.includes(filter))
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -48,6 +46,16 @@ const App = () => {
     setFilter(event.target.value)
   }
 
+  const initialPersons = () => {
+    axios
+    .get("http://localhost:3001/persons")
+    .then( response => {
+      const persons = response.data
+      setPersons(persons)
+    })
+  }
+
+  useEffect(initialPersons, [])
 
   return (
     <div>

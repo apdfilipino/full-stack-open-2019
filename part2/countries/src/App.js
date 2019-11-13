58788a7d518c37f5790ed4e4fa2d27ebcaa1a7ae
Aaron Filipino <apdfilipino@gmail.com>
2019-11-13 08:13:31 +0100

    Part 2, Countries Completed

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import Countries from './components/Countries'
+import SpecificCountry from './components/SpecificCountry'
 
 const App = () => {
 
   const [ countries, setCountries ] = useState([])
   const [ filter, setFilter ] = useState('')
+  const [ component, setComponent ] = useState('')
 
   const getCountries = () => {
     if( filter === '')
@@ -17,18 +19,32 @@ const App = () => {
         const restCountries = response.data
         setCountries(restCountries)
       })
+
+    if ( countries.length > 10 ) {
+      setComponent(<div>Too many matches, specify another filter</div>)
+    }
+    else if(countries.length > 1 && countries.length <= 10){
+      setComponent(<Countries countries={countries} showCountry={showCountry} />)
+    }
+    else if(countries.length === 1){
+      setComponent(<SpecificCountry country={countries[0]} />)
+    }
   }
 
   const filterChange = (event) => {
     setFilter(event.target.value)
   }
 
-  useEffect(getCountries, [filter])
+  const showCountry = (event) => {
+    setFilter(event.target.value)
+  }
+
+  useEffect(getCountries, [filter, countries])
 
   return (
     <div>
       find countries <input onChange={filterChange} />
-      <Countries countries={countries} />
+      {component}
     </div>
   )
 }
--- part2/countries/src/components/Countries.js
+++ part2/countries/src/components/Countries.js
@@ -1,30 +1,9 @@
 import React from 'react'
 import SpecificCountry from './SpecificCountry'
 
-const Countries = ( { countries } ) => {
-    if ( countries.length > 10 ) {
-        return (
-            <div>
-                Too many matches, specify another filter
-            </div>
-        )
-    }
-    else if ( countries.length > 1 && countries.length <= 10 ){
-        return (
-            countries.map( c => <div key={ c.alpha2Code }>{c.name} <button>show</button></div>)
-        )
-    }
-    else if ( countries.length === 1 ){
-        return (
-            <SpecificCountry country={countries[0]} /> 
-        )       
-    }
-    else {
-        return (
-            <div></div>
-        )
-    }
-
+const Countries = ( { countries, showCountry } ) => { 
+    const rows = countries.map( c => <div key={ c.alpha2Code }>{c.name} <button onClick={showCountry} value={c.name}>show</button></div>)
+    return rows
 }
 
 
